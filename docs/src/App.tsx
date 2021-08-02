f79b35597e28179bd782b60891cbf683fe11f890
Damian Stasik <visualfanatic@users.noreply.github.com>
2021-08-02 22:23:42 +0200

    WIP docs menu

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,10 +1,15 @@
-import { useRoutes } from 'solid-app-router';
-import Index from './index';
+import { useRoutes, Link } from 'solid-app-router';
+import Badge from './index';
+import Button from './button';
 
 const routes = [
   {
     path: '/',
-    component: Index,
+    component: Badge,
+  },
+  {
+    path: '/button',
+    component: Button,
   },
 ];
 
@@ -12,9 +17,24 @@ function App() {
   const Routes = useRoutes(routes);
 
   return (
-    <article class="prose prose-xl mx-auto mt-10">
-      <Routes />
-    </article>
+    <>
+      <div class="container mx-auto mt-10 flex">
+        <article class="prose prose-xl">
+          <h2>⚡ Zippy UI</h2>
+          <p class="lead -top-6 relative">Lightning fast components for Solid</p>
+        </article>
+      </div>
+      <div class="container mx-auto mt-10 flex">
+        <div class="flex flex-col w-96 pr-8 -ml-3">
+          <h5 class="px-3 mb-3 uppercase tracking-wide font-semibold text-xs text-gray-900">Components</h5>
+          <Link href="/" class="hover:bg-gray-300 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium">Badge</Link>
+          <Link href="/button" class="hover:bg-gray-300 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium">Button</Link>
+        </div>
+        <article class="prose prose-xl">
+          <Routes />
+        </article>
+      </div>
+    </>
   );
 }
 
--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -1,165 +1,9 @@
 import { Badge } from '@zippy-ui/core/dist/components/Badge';
-import { Button } from '@zippy-ui/core/dist/components/Button';
 
-export default function Index() {
+export default function Page() {
   return (
     <>
-      <h1>⚡ Zippy UI</h1>
-      <p class="lead -top-6 relative">Lightning fast components for Solid</p>
-
-      <h3>Button</h3>
-
-      <h4>Danger</h4>
-
-      <div class="border rounded-md border-1 border-gray-300 p-3 flex">
-        <Button
-          sx={{ mr: 2 }}
-          intent="danger"
-        >
-          solid danger
-        </Button>
-
-        <Button
-          variant="outlined"
-          intent="danger"
-          sx={{ mr: 2 }}
-        >
-          outlined danger
-        </Button>
-
-        <Button
-          variant="subtle"
-          intent="danger"
-        >
-          subtle danger
-        </Button>
-      </div>
-
-      <h4>Success</h4>
-
-      <div class="border rounded-md border-1 border-gray-300 p-3 flex">
-        <Button
-          sx={{ mr: 2 }}
-          intent="success"
-        >
-          solid success
-        </Button>
-
-        <Button
-          variant="outlined"
-          intent="success"
-          sx={{ mr: 2 }}
-        >
-          outlined success
-        </Button>
-
-        <Button
-          variant="subtle"
-          intent="success"
-        >
-          subtle success
-        </Button>
-      </div>
-
-      <h4>Warning</h4>
-
-      <div class="border rounded-md border-1 border-gray-300 p-3 flex">
-        <Button
-          sx={{ mr: 2 }}
-          intent="warning"
-        >
-          solid warning
-        </Button>
-
-        <Button
-          variant="outlined"
-          intent="warning"
-          sx={{ mr: 2 }}
-        >
-          outlined warning
-        </Button>
-
-        <Button
-          variant="subtle"
-          intent="warning"
-        >
-          subtle warning
-        </Button>
-      </div>
-
-      <h4>Info</h4>
-
-      <div class="border rounded-md border-1 border-gray-300 p-3 flex">
-        <Button
-          sx={{ mr: 2 }}
-          intent="info"
-        >
-          solid info
-        </Button>
-
-        <Button
-          variant="outlined"
-          intent="info"
-          sx={{ mr: 2 }}
-        >
-          outlined info
-        </Button>
-
-        <Button
-          variant="subtle"
-          intent="info"
-        >
-          subtle info
-        </Button>
-      </div>
-
-      <h4>None</h4>
-
-      <div class="border rounded-md border-1 border-gray-300 p-3 flex">
-        <Button sx={{ mr: 2 }}>solid none</Button>
-
-        <Button
-          variant="outlined"
-          sx={{ mr: 2 }}
-        >
-          outlined none
-        </Button>
-
-        <Button variant="subtle">subtle none</Button>
-      </div>
-
       <h3>Badge</h3>
-
-      <table>
-        <thead><tr><th>Prop</th><th>Default value</th><th>Values</th></tr></thead>
-        <tbody>
-          <tr>
-            <td><code>variant</code></td>
-            <td><code>solid</code></td>
-            <td><code>solid</code> | <code>outlined</code> | <code>subtle</code></td>
-          </tr>
-          <tr>
-            <td><code>size</code></td>
-            <td><code>default</code></td>
-            <td><code>small</code> | <code>default</code> | <code>large</code></td>
-          </tr>
-          <tr>
-            <td><code>intent</code></td>
-            <td><code>none</code></td>
-            <td><code>none</code> | <code>danger</code> | <code>warning</code> | <code>success</code> | <code>info</code></td>
-          </tr>
-          <tr>
-            <td><code>sx</code></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td><code>children</code></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
 
       <h4>Danger</h4>
 
@@ -307,6 +151,37 @@ export default function Index() {
           subtle none
         </Badge>
       </div>
+
+      <table>
+        <thead><tr><th>Prop</th><th>Default value</th><th>Values</th></tr></thead>
+        <tbody>
+          <tr>
+            <td><code>variant</code></td>
+            <td><code>solid</code></td>
+            <td><code>solid</code> | <code>outlined</code> | <code>subtle</code></td>
+          </tr>
+          <tr>
+            <td><code>size</code></td>
+            <td><code>default</code></td>
+            <td><code>small</code> | <code>default</code> | <code>large</code></td>
+          </tr>
+          <tr>
+            <td><code>intent</code></td>
+            <td><code>none</code></td>
+            <td><code>none</code> | <code>danger</code> | <code>warning</code> | <code>success</code> | <code>info</code></td>
+          </tr>
+          <tr>
+            <td><code>sx</code></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td><code>children</code></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
