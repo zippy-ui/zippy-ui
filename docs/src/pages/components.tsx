5b28cfe06ec9e42ed82bb0885ffa932a3cc1d7d9
Damian Stasik <visualfanatic@users.noreply.github.com>
2021-08-02 22:43:50 +0200

    Style active links in docs menu

--- docs/src/pages/components.tsx
+++ docs/src/pages/components.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from 'solid-app-router';
+import { Outlet, NavLink } from 'solid-app-router';
 
 export default function Components() {
   return (
@@ -7,18 +7,20 @@ export default function Components() {
         <h5 className="px-3 mb-3 uppercase tracking-wide font-semibold text-xs text-gray-900">
           Components
         </h5>
-        <Link
+        <NavLink
           href="/components/badge"
           class="hover:bg-gray-300 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+          activeClass="bg-blue-500 text-white hover:bg-blue-600 hover:text-white"
         >
           Badge
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           href="/components/button"
           class="hover:bg-gray-300 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+          activeClass="bg-blue-500 text-white hover:bg-blue-600 hover:text-white"
         >
           Button
-        </Link>
+        </NavLink>
       </div>
       <article className="prose prose-xl flex-1">
         <Outlet />
