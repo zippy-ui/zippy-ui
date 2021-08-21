a2a76208aa6cbac534b32faaf1e391b613051f8f
Damian Stasik <visualfanatic@users.noreply.github.com>
2021-08-21 23:10:02 +0200

    Use example box in badge docs

--- docs/src/pages/badge.tsx
+++ docs/src/pages/badge.tsx
@@ -1,200 +1,63 @@
 import { Badge } from '@zippy-ui/core/dist/components/Badge';
+import { ExampleBox } from '../components/ExampleBox';
 
 export default function Page() {
   return (
     <>
       <h3>Badge</h3>
 
-      <h4>Danger</h4>
-
-      <div className="border rounded-md border-1 border-gray-300 p-3 flex dark:border-gray-700">
-        <Badge
-          variant="outlined"
-          intent="danger"
-          sx={{
-            mr: 2,
-          }}
-        >
-          outlined danger
-        </Badge>
-
-        <Badge
-          variant="solid"
-          intent="danger"
-          sx={{
-            mr: 2,
-          }}
-        >
-          solid danger
-        </Badge>
-        <Badge variant="subtle" intent="danger">
-          subtle danger
-        </Badge>
-      </div>
-
-      <h4>Success</h4>
-
-      <div className="border rounded-md border-1 border-gray-300 p-3 flex dark:border-gray-700">
-        <Badge
-          variant="outlined"
-          intent="success"
-          sx={{
-            mr: 2,
-          }}
-        >
-          outlined success
-        </Badge>
-        <Badge
-          variant="solid"
-          intent="success"
-          sx={{
-            mr: 2,
-          }}
-        >
-          solid success
-        </Badge>
-        <Badge variant="subtle" intent="success">
-          subtle success
-        </Badge>
-      </div>
-
-      <h4>Warning</h4>
-
-      <div className="border rounded-md border-1 border-gray-300 p-3 flex dark:border-gray-700">
-        <Badge
-          variant="outlined"
-          intent="warning"
-          sx={{
-            mr: 2,
-          }}
-        >
-          outlined warning
-        </Badge>
-        <Badge
-          variant="solid"
-          intent="warning"
-          sx={{
-            mr: 2,
-          }}
-        >
-          solid warning
-        </Badge>
-        <Badge variant="subtle" intent="warning">
-          subtle warning
-        </Badge>
-      </div>
-
-      <h4>Info</h4>
-
-      <div className="border rounded-md border-1 border-gray-300 p-3 flex dark:border-gray-700">
-        <Badge
-          variant="outlined"
-          intent="info"
-          sx={{
-            mr: 2,
-          }}
-        >
-          outlined info
-        </Badge>
-        <Badge
-          variant="solid"
-          intent="info"
-          sx={{
-            mr: 2,
-          }}
-        >
-          solid info
-        </Badge>
-        <Badge variant="subtle" intent="info">
-          subtle info
-        </Badge>
-      </div>
-
-      <h4>None</h4>
-
-      <div className="border rounded-md border-1 border-gray-300 p-3 flex dark:border-gray-700">
-        <Badge
-          variant="outlined"
-          intent="none"
-          sx={{
-            mr: 2,
-          }}
-        >
-          outlined none
-        </Badge>
-        <Badge
-          variant="solid"
-          intent="none"
-          sx={{
-            mr: 2,
-          }}
-        >
-          solid none
-        </Badge>
-        <Badge variant="subtle" intent="none">
-          subtle none
-        </Badge>
-      </div>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Prop</th>
-            <th>Default value</th>
-            <th>Values</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <code>variant</code>
-            </td>
-            <td>
-              <code>solid</code>
-            </td>
-            <td>
-              <code>solid</code> | <code>outlined</code> | <code>subtle</code>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>size</code>
-            </td>
-            <td>
-              <code>default</code>
-            </td>
-            <td>
-              <code>small</code> | <code>default</code> | <code>large</code>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>intent</code>
-            </td>
-            <td>
-              <code>none</code>
-            </td>
-            <td>
-              <code>none</code> | <code>danger</code> | <code>warning</code> |{' '}
-              <code>success</code> | <code>info</code>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>sx</code>
-            </td>
-            <td />
-            <td />
-          </tr>
-          <tr>
-            <td>
-              <code>children</code>
-            </td>
-            <td />
-            <td />
-          </tr>
-        </tbody>
-      </table>
+      <ExampleBox
+        options={[
+          {
+            type: 'select',
+            default: 'solid',
+            items: [
+              { value: 'outlined', label: 'Outlined' },
+              { value: 'solid', label: 'Solid' },
+            ],
+            label: 'Variant',
+            name: 'variant',
+          },
+          {
+            type: 'select',
+            default: 'danger',
+            items: [
+              { value: 'danger', label: 'Danger' },
+              { value: 'success', label: 'Success' },
+              { value: 'warning', label: 'Warning' },
+              { value: 'info', label: 'Info' },
+              { value: 'none', label: 'None' },
+            ],
+            label: 'Intent',
+            name: 'intent',
+          },
+          {
+            type: 'select',
+            default: 'default',
+            items: [
+              { value: 'small', label: 'Small' },
+              { value: 'default', label: 'Default' },
+              { value: 'large', label: 'Large' },
+            ],
+            label: 'Size',
+            name: 'size',
+          },
+          {
+            type: 'text',
+            default: 'Badge label',
+            label: 'Content',
+            name: 'content',
+            min: 1,
+            max: 20,
+          },
+        ]}
+      >
+        {({ intent, size, variant, content }) => (
+          <Badge intent={intent} size={size} variant={variant}>
+            {content}
+          </Badge>
+        )}
+      </ExampleBox>
     </>
   );
 }
